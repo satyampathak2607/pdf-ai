aaf4ea6b0270ba1cb60751402e41b9521d8ff802
Satyam Pathak <satyam_24a12res598@iitp.ac.in>
2025-05-18 16:25:25 +0530

    Initial commit with Streamlit PDF chatbot

---
+++ app.py
@@ -0,0 +1,89 @@
+import fitz
+import streamlit as st
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import faiss
+import cohere
+
+co = cohere.Client("yNiOGAbn3zaiuAFjVj3pwW8e6eZ2mjIJmf2VcgKh")
+
+def generate_answer(question, context):
+    prompt = f"Context: {context}\n\nQuestion: {question}\nAnswer:"
+    
+    response = co.generate(
+        model='command-xlarge-nightly',
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.5,
+        k=0,
+        stop_sequences=["\n"]
+    )
+    return response.generations[0].text.strip()
+def extract_text_from_pdf(pdf_file):
+    text = ""
+    with fitz.open(pdf_file) as doc:
+        for page in doc:
+            text += page.get_text()
+    return text
+
+def chunk_text(text, max_tokens = 500):
+    sentences = text.split('. ')
+    chunks = []
+    current_chunk = ""
+    for sentence in sentences:
+       if len(current_chunk + sentence) <= max_tokens:
+           current_chunk += sentence + '.'
+       else:
+           chunks.append(current_chunk.strip())
+           current_chunk = sentence + '.'
+
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    return chunks
+
+def load_model():
+    return SentenceTransformer("all-MiniLM-L6-v2")
+
+model = load_model()
+
+
+
+           
+
+#streamlit ui
+
+st.title("PDF TO AI Chatbot")
+uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
+
+if uploaded_file is not None:
+    st.success("File uploaded successfully!")
+
+    text= extract_text_from_pdf(uploaded_file)
+
+    st.subheader("Extracted Text:")
+    st.write(text[:3000])
+    
+
+    chunks = chunk_text(text)
+    st.info(f" Total Chunks: {len(chunks)}")
+
+    embeddings = model.encode(chunks)
+
+    dim = embeddings.shape[1]
+    index = faiss.IndexFlatL2(dim)
+    index.add(np.array(embeddings))
+
+    st.success("Embeddings created and indexed successfully!")
+
+
+    st.subheader("💬 Ask your PDF a question")
+    user_question = st.text_input("Type your question:")
+    if user_question:
+        question_embedding = model.encode([user_question])
+        D, I = index.search(np.array(question_embedding), k=1)
+        answer = chunks[I[0][0]]
+        answer = generate_answer(user_question, answer)
+
+        st.success("🧠 Best match from PDF:")
+        st.write(answer)
+
